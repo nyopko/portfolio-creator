e4e0af7530b05797318770e13aee2c7dc4e151cd
nyopko <44181737+nyopko@users.noreply.github.com>
2021-03-17 16:17:25 -0400

    Added moving letters src, js, and css.

--- assets/index.js
+++ assets/index.js
@@ -2,4 +2,38 @@ $( document ).ready(function() {
     $("#button-click").click(function() {
         $("#test-id").attr("class", "test1");
     });
+
+
+// Moving Letters Specific JS
+
+// Title-Specific
+
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  });
+
+// Description-Specific
+var textWrapper = document.querySelector('.ml12');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml12 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  });
+
+
 });
